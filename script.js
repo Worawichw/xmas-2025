68c4efe70a3cc053c4ab5c8711f7d5d1515d8f9d
Worawich <paiworawich@gmail.com>
2025-12-25 17:25:37 +0700

    FIRST FINAL

--- script.js
+++ script.js
@@ -1,34 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     let num = 10;
     const displayText = document.getElementById('lower');
-    const button = document.getElementById('action_button');
+    const action_button = document.getElementById('action_button');
+    const yes_button = document.getElementById('choose-yes');
+    const no_button = document.getElementById('choose-no');
+    const bf = document.getElementById('bf');
+    const gf = document.getElementById('gf');
 
     
     function change() {
+        bf.src = 'assets/bf-yay.png';
+        gf.src = 'assets/gf-yay.png';
+
         num -= 1;
         if (num<=0){
             num=0;
-            const content = document.getElementById('content')
-            content.classList.add('show')
+            const content = document.getElementById('content');
+            content.classList.add('show');
             
-            document.getElementById('non-content').style.filter="blur(8px)"
-            button.remove()
+            document.getElementById('non-content').style.filter="blur(15px)";
+            action_button.remove();
         }
 
-        displayText.textContent = `count: ${num}`;
+        displayText.textContent = `กดอีก ${num} ครั้ง`;
 
         displayText.classList.remove('pop');
         void displayText.offsetWidth; 
         displayText.classList.add('pop');
 
-        button.classList.remove('changebutton');
+        action_button.classList.remove('changebutton');
         void displayText.offsetWidth; 
-        button.classList.add('changebutton');
+        action_button.classList.add('changebutton');
 
+        setTimeout(yay,500);
         console.log(num);
+        
     }
 
-    if (button) button.addEventListener('click', change);
+    function showSecret(){
+        const secret = document.getElementById("secret");
+        secret.classList.add('show');
+        content.classList.remove('show');
+        content.classList.add('hide');
+    }
+
+    function yay(){
+        bf.src = 'assets/bf.png';
+        gf.src = 'assets/gf.png';
+    }
+    
+
+    if (action_button) action_button.addEventListener('click', change);
+    if (yes_button) yes_button.addEventListener('click', showSecret);
+    if (no_button) no_button.addEventListener('click', showSecret);
 
     
 });
